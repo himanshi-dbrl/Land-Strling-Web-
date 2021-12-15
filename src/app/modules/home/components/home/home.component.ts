d18d9512a95f256c7a113f22ee6cf1bd364c96ec
Himanshi Dabral <himanshi.dabral@mobilecoderz.com>
2021-12-15 19:24:26 +0530

    getting location by pointing map, set markers on map

--- src/app/modules/home/components/home/home.component.ts
+++ src/app/modules/home/components/home/home.component.ts
@@ -1,10 +1,12 @@
+import { HttpClient } from "@angular/common/http";
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { AddPropertyComponent } from '../add-property/add-property.component';
 import { Router } from '@angular/router';
-import { HomeService } from 'src/app/modules/service/home.service';
 import { fromEvent } from 'rxjs';
-import { HttpClient } from "@angular/common/http";
+import { HomeService } from 'src/app/modules/service/home.service';
+
+import { AddPropertyComponent } from '../add-property/add-property.component';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -16,6 +18,12 @@ export class HomeComponent implements OnInit, AfterViewInit {
   showFooterFilter: boolean = false;
   lat: any;
   lng: any;
+  latit: any;
+  lngit: any;
+   mark =[];
+
+  
+  data: object = null;
   city: string = null;
   country: string = null;
   state: string = null;
@@ -23,6 +31,8 @@ export class HomeComponent implements OnInit, AfterViewInit {
   propertyType: string = null;
   propertyStatus: string = null;
   userId: any = '0054K000001L99mQAC';
+  selectedLat: Number = 0;
+selectedLng: Number = 0;
   constructor(
     // public dialog: MatDialog
     private http: HttpClient,
@@ -32,46 +42,57 @@ export class HomeComponent implements OnInit, AfterViewInit {
   ) { }
 
   ngOnInit(): void {
-    this.homeService.getListedProperty('0054K000001L99mQAC').subscribe(res => {
-      console.log("all property is", res);
-    })
-
-
     let res = this.homeService.getUserLocation().then(res => {
       console.log('res is', res);
-      this.lat = res.lat;
-      this.lng = res.lng;
+      this.latit = res.lat;
+      this.lngit = res.lng;
     });
+    this.homeService.getListedProperty('0054K000001L99mQAC').subscribe(res => {
+      console.log("all property is", res);
+ 
+      //console.log("all property is", res.data[0].pba__Longitude_pb__c); 
+       var i=1;
+      for (let data of res.data) { 
+        console.log(data);
+        var ov;
+        
+        ov=
+          { 
+           lat: this.latit,
+           lng: this.lngit,
+           label: '',
+           iconUrl: "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|4286f4",
+            no:i
+         };
+         i++;
+          this.mark.push(ov);
+        if(data){
+          ov={
+                lat:data.pba__Latitude_pb__c,
+                lng:data.pba__Longitude_pb__c,
+                label: data.pba__City_pb__c,
+                iconUrl: "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|FD0002"
+          } 
+          this.mark.push(ov);
+
+        }
+
+
+       }
+       console.log("our final outpt is",this.mark);
+
+    })
   }
+
+ 
 
   ngAfterViewInit(): void {
     this.onDoubleClickMap();
   }
-
-  markers = [
-    {
-      lat: 21.1594627,
-      lng: 72.6822083,
-      label: 'Surat',
-      iconUrl: "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|4286f4"
-    },
-
-    {
-      lat: 23.0204978,
-      lng: 72.4396548,
-      label: 'Ahmedabad',
-      iconUrl: "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|4286f4"
-    },
-    {
-      lat: 22.2736308,
-      lng: 70.7512555,
-      label: 'Rajkot',
-      iconUrl: "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|FD0002"
-    }
-
-  ];
+ 
 
   onDoubleClickMap() {
+
     const map = document.getElementById('map');
     fromEvent(map, 'dblclick').subscribe(event => {
       console.log(event);
@@ -112,4 +133,14 @@ export class HomeComponent implements OnInit, AfterViewInit {
     this.showHeadFilter = false
     this.showFooterFilter = true;
   }
+
+  markerClicked(lat:number,lng:number){
+this.selectedLat = lat;
+this.selectedLng = lng;
+console.log('getting clicked address latitude',this.selectedLat );
+console.log('getting clicked address longitude',this.selectedLng);
+  console.log('enter' )  ;
+  }
+
+  
 }
